5b90bbf2f3f27356bf1ead63f6a8019203c304af
Jean Lopes <jean.llopes@outlook.com>
2019-02-03 18:10:42 -0200

    Create methods

--- CSharp_Delegates/Program.cs
+++ CSharp_Delegates/Program.cs
@@ -4,9 +4,30 @@ namespace CSharp_Delegates
 {
     class Program
     {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void WriteConsoleLine(string text)
+        {
+            Console.WriteLine($"Start {nameof(WriteConsoleLine)} with parameter: {text}.");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Start {nameof(CSharp_Delegates)}");
+
+            //CREATE DELEGATE Func<T> Delegate
+            Console.WriteLine($"Start Func<T> Delegate");
+            Func<int, int, int> sumDelegate = Sum;
+            Console.WriteLine($"Result: {sumDelegate(10,30)}");
+            Console.ReadKey();
+            
+            //CREATE DELEGATE Action<T> Delegate
+            Console.WriteLine("Start Action<T> Delegate");
+            Action<string> writeConsoleLineDelegate = WriteConsoleLine;
+            Console.ReadKey();
         }
     }
 }
